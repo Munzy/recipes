52cd6c938eff80508f038753947ed34c6af43907
Munzy <cm@munroenet.com>
2018-03-06 11:24:16 -0800

    Added UDP6 functionality.

--- recipes/debian/snmpd-install.sh
+++ recipes/debian/snmpd-install.sh
@@ -51,8 +51,9 @@ wait
 COMMUNITYSTRING=`openssl rand -base64 24`
 wait
 
-echo '# Change RANDOMSTRINGGOESHERE to your preferred SNMP community string' > /etc/snmp/snmpd.conf
+echo "agentAddress udp:161,udp6:161" >> /etc/snmp/snmpd.conf
 echo "com2sec readonly  default         ${COMMUNITYSTRING}" >> /etc/snmp/snmpd.conf
+echo "com2sec6 readonly  default         ${COMMUNITYSTRING}" >> /etc/snmp/snmpd.conf
 echo "" >> /etc/snmp/snmpd.conf
 echo "group MyROGroup v2c        readonly" >> /etc/snmp/snmpd.conf
 echo "view all    included  .1" >> /etc/snmp/snmpd.conf
